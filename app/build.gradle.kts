bf031301d25fdce308bc85b4b78aaac7380ac353
Robert <bigbob1991@gmail.com>
2022-02-14 12:37:39 +0400

    dependencies update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,7 +70,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.1.0-alpha04"
+        kotlinCompilerExtensionVersion = "1.2.0-alpha03"
     }
     kapt {
         correctErrorTypes = true
@@ -92,19 +92,19 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.40.5")
     kapt("com.google.dagger:hilt-android-compiler:2.40.5")
 
-    implementation("androidx.activity:activity-compose:1.5.0-alpha01")
+    implementation("androidx.activity:activity-compose:1.5.0-alpha02")
     implementation("androidx.appcompat:appcompat:1.4.1")
     implementation("androidx.constraintlayout:constraintlayout-compose:1.0.0")
     implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
-    implementation("androidx.navigation:navigation-compose:2.5.0-alpha01")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.1")
+    implementation("androidx.navigation:navigation-compose:2.5.0-alpha02")
     implementation("androidx.paging:paging-compose:1.0.0-alpha14")
 
-    implementation("androidx.compose.compiler:compiler:1.2.0-alpha02")
-    implementation("androidx.compose.ui:ui:1.2.0-alpha02")
-    implementation("androidx.compose.material:material:1.2.0-alpha02")
-    implementation("androidx.compose.ui:ui-tooling:1.2.0-alpha02")
+    implementation("androidx.compose.compiler:compiler:1.2.0-alpha03")
+    implementation("androidx.compose.ui:ui:1.2.0-alpha03")
+    implementation("androidx.compose.material:material:1.2.0-alpha03")
+    implementation("androidx.compose.ui:ui-tooling:1.2.0-alpha03")
 
     implementation("io.coil-kt:coil-compose:1.4.0")
 
@@ -115,5 +115,5 @@ dependencies {
 
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.2.0-alpha02")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.2.0-alpha03")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.1.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30")
+        classpath("com.android.tools.build:gradle:7.1.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.40")
     }
 }
