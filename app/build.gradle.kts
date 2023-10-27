7b2891061c714ae5c030b0d3be733d9683d280e7
Robert <bigbob1991@gmail.com>
2023-10-27 16:53:46 +0400

    dependencies updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,18 +4,18 @@ plugins {
     id("com.android.application")
     kotlin("android")
     kotlin("kapt")
-    kotlin("plugin.serialization") version "1.8.20"
+    kotlin("plugin.serialization") version "1.9.10"
     id("dagger.hilt.android.plugin")
     id("kotlin-parcelize")
 }
 
 android {
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.robertlevonyan.composable.newsapp"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -71,7 +71,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.7"
+        kotlinCompilerExtensionVersion = "1.5.3"
     }
     namespace = "com.robertlevonyan.composable.newsapp"
     kapt {
@@ -81,42 +81,42 @@ android {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
-    implementation("io.ktor:ktor-client-core:2.3.0")
-    implementation("io.ktor:ktor-client-android:2.3.0")
-    implementation("io.ktor:ktor-client-content-negotiation:2.3.0")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("io.ktor:ktor-client-core:2.3.5")
+    implementation("io.ktor:ktor-client-android:2.3.5")
+    implementation("io.ktor:ktor-client-content-negotiation:2.3.5")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.5")
 
     implementation("com.google.accompanist:accompanist-flowlayout:0.23.0")
     implementation("com.google.accompanist:accompanist-insets:0.23.0")
     implementation("com.google.accompanist:accompanist-pager:0.18.0")
-    implementation("com.google.android.material:material:1.9.0-rc01")
-    implementation("com.google.dagger:hilt-android:2.45")
-    kapt("com.google.dagger:hilt-android-compiler:2.45")
+    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.dagger:hilt-android:2.48")
+    kapt("com.google.dagger:hilt-android-compiler:2.48")
 
-    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.activity:activity-compose:1.8.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
-    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.navigation:navigation-compose:2.7.0-alpha01")
-    implementation("androidx.paging:paging-compose:1.0.0-alpha19")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.navigation:navigation-compose:2.7.4")
+    implementation("androidx.paging:paging-compose:3.3.0-alpha02")
 
-    implementation("androidx.compose.compiler:compiler:1.4.7")
-    implementation("androidx.compose.ui:ui:1.5.0-beta01")
-    implementation("androidx.compose.material:material:1.5.0-beta01")
-    implementation("androidx.compose.ui:ui-tooling:1.5.0-beta01")
+    implementation("androidx.compose.compiler:compiler:1.5.3")
+    implementation("androidx.compose.ui:ui:1.6.0-alpha08")
+    implementation("androidx.compose.material:material:1.6.0-alpha08")
+    implementation("androidx.compose.ui:ui-tooling:1.6.0-alpha08")
 
-    implementation("io.coil-kt:coil-compose:2.2.2")
+    implementation("io.coil-kt:coil-compose:2.4.0")
 
-    implementation("com.airbnb.android:lottie-compose:6.0.0")
-    implementation("com.robertlevonyan.compose:materialchip:3.0.5")
+    implementation("com.airbnb.android:lottie-compose:6.1.0")
+    implementation("com.robertlevonyan.compose:materialchip:3.0.6")
 
     testImplementation("junit:junit:4.13.2")
 
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.0-beta01")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.6.0-alpha08")
 }
